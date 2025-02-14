fa25623e9798e43abd9d0c429db28c1d21abca4b
rigellin73 <rigellin73@gmail.com>
2025-02-14 17:28:40 +0100

    add text to flash card

--- flash_card.py
+++ flash_card.py
@@ -9,6 +9,18 @@ BGR_COLOR = "#B1DDC6"
 BGR_FRONT = "images/card_front.png"
 BGR_BACK = "images/card_back.png"
 
+# Text settings
+T_LANG_POSX = C_WIDTH / 2
+T_LANG_POSY = 150
+T_LANG_TEXT_FRONT = "Svenska"
+T_LANG_TEXT_BACK = "English"
+T_LANG_FONT = ("Arial", 40, "italic")
+
+T_WORD_POSX = C_WIDTH / 2
+T_WORD_POSY = 263
+T_WORD_TEXT_DEFAULT = "Ord"
+T_WORD_FONT = ("Arial", 60, "bold")
+
 class FlashCard(tk.Canvas):
     def __init__(self):
         super().__init__()
@@ -17,3 +29,6 @@ class FlashCard(tk.Canvas):
 
         self.config(width=C_WIDTH, height=C_HEIGHT, bg=BGR_COLOR, highlightthickness=0)
         self.create_image(C_WIDTH / 2, C_HEIGHT / 2, image=self.bgr_image_back)
+
+        self.lang_text = self.create_text(T_LANG_POSX, T_LANG_POSY, text=T_LANG_TEXT_FRONT, font=T_LANG_FONT)
+        self.word_text = self.create_text(T_WORD_POSX, T_WORD_POSY, text=T_WORD_TEXT_DEFAULT, font=T_WORD_FONT)
